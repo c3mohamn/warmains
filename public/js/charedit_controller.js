76cef085d87cf83aa73143260d280591fd827c6c
Nasir M <nasir.m23@hotmail.com>
2017-05-03 02:57:51 -0400

    Added more restrictions to equipping items.

--- public/js/charedit_controller.js
+++ public/js/charedit_controller.js
@@ -14,42 +14,29 @@ editApp.controller('editctrl', ['$scope', '$http', '$location', function($scope,
       // Set the item slot that we are looking for.
       $scope.set_slot = function(slot) {
         $scope.slot = slot;
+        $scope.search_val = '';
       }
+
       // Set the color of the item name based on its quality.
       $scope.item_quality = function(quality) {
-        if (quality == 'Epic')
-          return {'color': 'purple'}
-        else if (quality == 'Rare')
-          return {'color': 'blue'}
-        else if (quality == 'Legendary')
-          return {'color': 'orange'}
-        else
-          return {'color': 'black'}
+        if      (quality == 'Epic')         return {'color': 'purple'}
+        else if (quality == 'Rare')         return {'color': 'blue'}
+        else if (quality == 'Legendary')    return {'color': 'orange'}
+        else                                return {'color': 'black'}
       }
       // Set the color of class names
       $scope.class_color = function(item_class) {
-        if (item_class == 'Warrior')
-          return {'color': '#C79C6E'}
-        else if (item_class == 'Warlock')
-          return {'color': '#9482C9'}
-        else if (item_class == 'Shaman')
-          return {'color': '#0070DE'}
-        else if (item_class == 'Rogue')
-          return {'color': '#FFF569'}
-        else if (item_class == 'Paladin')
-          return {'color': '#F58CBA'}
-        else if (item_class == 'Mage')
-          return {'color': '#69CCF0	'}
-        else if (item_class == 'Hunter')
-          return {'color': '#ABD473'}
-        else if (item_class == 'Death Knight')
-          return {'color': '#C41F3B'}
-        else if (item_class == 'Druid')
-          return {'color': '#FF7D0A'}
-        else if (item_class == 'Priest')
-          return {'color': '#FFFFFF'}
-        else
-          return {'color': 'black'}
+        if      (item_class == 'Warrior')       return {'color': '#C79C6E'}
+        else if (item_class == 'Warlock')       return {'color': '#9482C9'}
+        else if (item_class == 'Shaman')        return {'color': '#0070DE'}
+        else if (item_class == 'Rogue')         return {'color': '#FFF569'}
+        else if (item_class == 'Paladin')       return {'color': '#F58CBA'}
+        else if (item_class == 'Mage')          return {'color': '#69CCF0'}
+        else if (item_class == 'Hunter')        return {'color': '#ABD473'}
+        else if (item_class == 'Death Knight')  return {'color': '#C41F3B'}
+        else if (item_class == 'Druid')         return {'color': '#FF7D0A'}
+        else if (item_class == 'Priest')        return {'color': '#FFFFFF'}
+        else                                    return {'color': 'black'}
       }
 
       // select an item from results table
@@ -58,25 +45,49 @@ editApp.controller('editctrl', ['$scope', '$http', '$location', function($scope,
         selected_item = item;
       }
 
-      // TODO: ADD CONDITIONS FOR TYPES OF ARMOR THAT CAN BE EQUIPPED BY CERTAIN CLASS
+      /* Equips an item to corresponding slot selected.
+      *  - Check if an item is selected first.
+      *  - Check if the selected item can be equipped in corresponding slot.
+      *  - Check if the current character can wield that type of item.
+      *  - Make sure we cannot equip an offhand when a twohand is equipped.
+      */
       $scope.equip_item = function() {
+        var error_msg_1 = 'Select an item before trying to equip.';
+        var error_msg_2 = 'Invalid item selection.';
+        var error_msg_4 = 'Cannot equip an offhand while using a twohand.';
+        $scope.message = '';
+        var temp_slot = $scope.slot;
+        temp_slot = remove_trailing_number(temp_slot);
+
         if (selected_item) {
-          if (selected_item.Slot == $scope.slot) {
-              if (can_wield($scope.character, selected_item)) {
+          var error_msg_3 = 'You cannot equip ' + selected_item.Type + '.';
+
+          //Special cases are so we can equip TwoHand in MainHand slot etc.
+          if (compare_slot(temp_slot, selected_item, $scope.character)) {
+            if (can_wield($scope.character, selected_item)) {
+              if (!(char_items['mainhand'] &&
+                char_items['mainhand'].Slot == 'TwoHand' &&
+                $scope.character.class != 'warrior' && temp_slot != 'MainHand')) {
+
+                console.log(is_unique($scope.slot, selected_item));
+                // Equip the item and change the icon image.
                 char_items[$scope.slot.toLowerCase()] = selected_item;
                 set_slot_image($scope.slot);
-              } else {
-                $scope.message = 'You cannot equip ' + selected_item.Type + '.';
-              }
-          } else {
-            $scope.message = 'Select the correct item slot!';
-          }
-        } else {
-          $scope.message = 'Select an item before trying to equip.';
-        }
+              } else { $scope.message =  error_msg_4; }
+            } else { $scope.message = error_msg_3; }
+          } else { $scope.message = error_msg_2; }
+        } else { $scope.message = error_msg_1; }
       }
-      // Function to find items for a given set item slot.
+
+      /* Function to find items for a given item slot and search value.
+      *  - Make sure a item slot is selected before searching.
+      *  - Make sure search value is above 2 characters long.
+      *  - Remove trailing numbers after the slot (ex. Finger1/Trinket2)
+      *  - Remove items that are not meant for the current character's class.
+      *  - Show result table only when we have matching items for search value.
+      */
       $scope.finditems = function() {
+        var show_results = false; // used to show or hide results table
 
         if (!$scope.slot) {
           $scope.message = 'Select a item slot before searching for items.';
@@ -86,61 +97,46 @@ editApp.controller('editctrl', ['$scope', '$http', '$location', function($scope,
           $scope.message = '';
 
           var temp_slot = $scope.slot;
-          var last_char = temp_slot[temp_slot.length - 1];
-
-          // Remove the 1 from Finger1 when searching for items.
-          if (last_char == '1' || last_char == '2') {
-            temp_slot = temp_slot.slice(0, temp_slot.length - 1);
-          }
-          //var all_items = [];
+          temp_slot = remove_trailing_number(temp_slot);
 
           $http.get('/wowdata/' + temp_slot + '.json').then(function(response){
-              var all_items = response.data.items;
-              var matching_items = [];
-              var search_val = $scope.search_val.toLowerCase();
+            var all_items = response.data.items;
+            var matching_items = [];
+            var search_val = $scope.search_val.toLowerCase();
 
-              //TODO: Add TwoHand and OneHand weapons to MainHand list.
+            // loop through all items
+            angular.forEach(all_items, function(item, key){
+              var item_name = item.Name.toLowerCase();
+              var item_id = item.Id;
 
-              // loop through all items
-              angular.forEach(all_items, function(item, key){
-                // finding matching items to search value
-                var item_name = item.Name.toLowerCase();
-                var item_id = item.Id;
-                if ($scope.search_type) {
-                  /* Filter out the items that have a required class that does
-                     not match our chars */
-                  if (item.RequiredClasses &&
-                    item.RequiredClasses.toLowerCase().indexOf($scope.character.class) > -1
-                    || !item.RequiredClasses) {
-                    if ($scope.search_type == 'Name' && item_name.indexOf(search_val) > -1) {
-                      matching_items.push(item);
-                    } else if (item_id.indexOf(search_val) == 0) {
-                      matching_items.push(item);
-                    }
+              if ($scope.search_type) {
+                /* Filter out the items that have a required class that does
+                   not match our chars */
+                if (item.RequiredClasses &&
+                  item.RequiredClasses.toLowerCase().indexOf($scope.character.class) > -1
+                  || !item.RequiredClasses) {
+                  // finding matching items based on item name
+                  if ($scope.search_type == 'Name' && item_name.indexOf(search_val) > -1) {
+                    matching_items.push(item);
+                    show_result = true;
+                  } // finding matching items based on item ID
+                  else if (item_id.indexOf(search_val) == 0) {
+                    matching_items.push(item);
+                    show_result = true;
                   }
-                } else {
-                  $scope.message = 'Selected a search by.';
                 }
-              });
-
-              // Hide / show result table based on whether there are any results
-              $scope.show_result_table = function() {
-                if (matching_items.length > 0)
-                  return {'visibility': 'visible'}
-                else
-                  return {'visibility': 'hidden'}
+              } else {
+                $scope.message = 'Selected a search by.';
               }
-              $scope.items = matching_items;
+            });
+            // Hide or show result table based on whether there are any results
+            $scope.show_result_table = function() {
+              if (show_result)  return {'visibility': 'visible'}
+              else              return {'visibility': 'hidden'}
+            }
+            $scope.items = matching_items;
           });
         }
-      }
-
-      //TODO: Create a function to search for items in JSON file.
-      // Retrieves list of all items from item slot
-      $scope.get_items = function(item_slot) {
-        $http.get('/wowdata/' + item_slot + '.json').then(function(response) {
-          return respone.data.items;
-        });
       }
 
       // find the current character in database - see if it actually
--- public/js/charedit_helper.js
+++ public/js/charedit_helper.js
@@ -1,4 +1,4 @@
-/* ------ Useful Variables ------ */
+          /* ------ Useful Variables ------ */
 
 // Getting the data for the character from the URL
 var cur_url = window.location.href;
@@ -41,7 +41,7 @@ var class_wield_type = {
   shaman: {
     armor: ['Cloth', 'Leather', 'Mail', 'None'],
     weapon: ['None', 'Shield', 'Totem', 'OneHandAxe', 'TwoHandAxe',
-            'OneHandMace', 'TwoHandMace', 'Dagger', 'Polearm', 'Staff']
+            'OneHandMace', 'TwoHandMace', 'Dagger', 'Staff']
   },
   hunter: {
     armor: ['Cloth', 'Leather', 'Mail', 'None'],
@@ -76,9 +76,83 @@ var class_wield_type = {
 // Stores the currently selected item.
 var selected_item = null;
 
-/* ------ Useful Functions ------ */
+          /* ------ Useful Functions ------ */
 
-// Setting the icon img of the corresponding slot to what is in char_items
+/* This function is used to remove the 1/2 at the end of finger/trinket. */
+function remove_trailing_number(slot) {
+  var last_char = slot[slot.length - 1];
+
+  // Remove the 1 from Finger1 when searching for items.
+  if (last_char == '1' || last_char == '2') {
+    return slot.slice(0, slot.length - 1);
+  } else return slot
+}
+
+/* Return whether item slot is a weapon or not. */
+function is_weapon_slot(item_slot) {
+  weapons_slot = ['MainHand', 'OffHand', 'OneHand', 'TwoHand', 'Ranged'];
+  return (weapons_slot.indexOf(item_slot) >= 0);
+}
+
+/* Return whether the given slots are equal.
+*  Except for the cases where it is MainHand, OffHand or if character is a
+*  warrior (can dual wield two TwoHand).
+*  slot1: the currently selected slot
+*  sel_item: the currently selected item to be put into slot1
+*  char: the current characte profile
+*/
+function compare_slot(slot1, sel_item, char) {
+  var slot2 = sel_item.Slot;
+  // for armor
+  var case1 = slot1 == slot2;
+  // to allow two handers to be equipped by mainhand
+  var case2 = slot1 == 'MainHand' && (slot2 == 'TwoHand' || slot2 == 'OneHand');
+  // to allow one handers to be equipped by offhand
+  var case3 = slot1 == 'OffHand' && slot2 == 'OneHand';
+  // to allow warriors to dual wield
+  var case4 = slot1 == 'OffHand' && char.class == 'warrior' &&
+              slot2 == 'TwoHand';
+
+  // to prevent two handers being equipped after a offhand has been equipped.
+  var case5 = true;
+  if (char_items.offhand && slot1 == 'MainHand' && slot2 == 'TwoHand' &&
+      char.class != 'warrior')
+    case5 = false;
+  else case5 = true;
+
+  // to prevent warriors from dual wielding with polearm/staff
+  var case6 = true;
+  if (char_items.mainhand && ((char_items.mainhand.Type == 'Polearm' ||
+      char_items.mainhand.Type == 'Staff') && (slot2 == 'TwoHand'
+      && slot1 == 'OffHand')))
+    case6 = false;
+  else case6 = true;
+
+  return (case1 || case2 || case3 || case4) && case5 && case6;
+}
+
+/* Make sure that two unique rings/trinkets are not equipped at the same time.
+*  slot1: the currently selected slot.
+*  sel_item: the currently selected item to be put into slot1.
+*/
+function is_unique(slot1, sel_item) {
+  if (slot1 == 'Finger1') {
+    console.log('Finger1.');
+    if (char_items.finger2 && char_items.finger2.Unique) {
+
+    }
+  } else if (slot1 == 'Finger2') {
+    console.log('Finger2.');
+  } else if (slot1 == 'Trinket1') {
+    console.log('Trinket1.');
+  } else if (slot1 == 'Trinket2') {
+    console.log('Trinket2.');
+  } else return false;
+
+}
+
+
+/* Setting the icon img of the corresponding slot to what is in char_items */
 function set_slot_image(slot) {
   slot = slot.toLowerCase();
   $('#' + slot + '_slot').css('background-image', 'url(' +
@@ -86,25 +160,25 @@ function set_slot_image(slot) {
   '.jpg)');
 }
 
-// Check if the current character can wield the corresponding item.
-// Will need to make sure that characters do not dual weild two handers.
+/* Check if the current character can wield the corresponding item.
+*  Will need to make sure that characters do not dual wield two handers.
+*/
 function can_wield(character, item) {
   var char_class = character.class;
   var item_type = item.Type;
   var item_slot = item.Slot;
-  var weapons_slot = ['MainHand', 'OffHand', 'OneHand', 'TwoHand', 'Ranged'];
 
   // Check if this is a weapon or not.
-  if (weapons_slot.indexOf(item_slot) < 0) {
+  if (!is_weapon_slot(item_slot)) {
     // Check if the current character can equip that armor type.
     if (class_wield_type[char_class].armor.indexOf(item_type) < 0) {
-      console.log('Character cannot wear ', item_type);
+      console.log('Cannot wear armor: ', item_type);
       return false;
     }
   } else {
     // Check if the current character can equip that weapon type.
     if (class_wield_type[char_class].weapon.indexOf(item_type) < 0) {
-      console.log('Character cannot wear ', item_type);
+      console.log('Cannot wear weapon: ', item_type, class_wield_type[char_class].weapon);
       return false;
     }
   }
