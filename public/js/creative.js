d3d048c5c29fe7e3b72953e5b1b1e7a1b3a1775d
c3mohamn <nasir.m23@hotmail.com>
2017-05-29 17:31:05 -0400

    more changes, to visual aspect of site.

--- public/js/creative.js
+++ public/js/creative.js
@@ -16,11 +16,6 @@
         offset: 51
     });
 
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
-
     // Offset for Main Navigation
     $('#mainNav').affix({
         offset: {
